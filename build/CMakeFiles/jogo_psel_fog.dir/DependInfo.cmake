5f809f71c428892f8994fc163f5f989818036d80
luisooliveirasz <luisooliveirasz@gmail.com>
2026-05-10 00:55:52 -0300

    Switch to 3d snake game

---
+++ build/CMakeFiles/jogo_psel_fog.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/luis/Dev/jogo_psel_fog/src/glad.c" "CMakeFiles/jogo_psel_fog.dir/src/glad.c.o" "gcc" "CMakeFiles/jogo_psel_fog.dir/src/glad.c.o.d"
+  "/home/luis/Dev/jogo_psel_fog/src/main.c" "CMakeFiles/jogo_psel_fog.dir/src/main.c.o" "gcc" "CMakeFiles/jogo_psel_fog.dir/src/main.c.o.d"
+  "/home/luis/Dev/jogo_psel_fog/src/stb_image.c" "CMakeFiles/jogo_psel_fog.dir/src/stb_image.c.o" "gcc" "CMakeFiles/jogo_psel_fog.dir/src/stb_image.c.o.d"
+  "" "jogo_psel_fog" "gcc" "CMakeFiles/jogo_psel_fog.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/jogo_psel_fog.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jogo_psel_fog.dir/link.d"
+  "CMakeFiles/jogo_psel_fog.dir/src/glad.c.o"
+  "CMakeFiles/jogo_psel_fog.dir/src/glad.c.o.d"
+  "CMakeFiles/jogo_psel_fog.dir/src/main.c.o"
+  "CMakeFiles/jogo_psel_fog.dir/src/main.c.o.d"
+  "CMakeFiles/jogo_psel_fog.dir/src/stb_image.c.o"
+  "CMakeFiles/jogo_psel_fog.dir/src/stb_image.c.o.d"
+  "jogo_psel_fog"
+  "jogo_psel_fog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jogo_psel_fog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
